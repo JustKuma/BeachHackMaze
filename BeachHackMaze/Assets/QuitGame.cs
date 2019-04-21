0532f734a9f7dbf57c638ea3e32ceafa46b33a18
crkobel <crkobel@verizon.net>
2019-04-21 08:04:24 -0700

    Implemented main menu, made play again option after "end game" Need to make the buttons clickable

---
+++ BeachHackMaze/Assets/QuitGame.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class QuitGame : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Maze Scene");
+    }
+
+    public void QuitTheGame()
+    {
+        Debug.Log("QUIT!");
+        Application.Quit();
+    }
+}
