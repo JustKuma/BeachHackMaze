8b311f40456d037f1d0b4bf167b03d76ca5baa9e
Justin <justij4@uci.edu>
2019-04-21 08:59:13 -0700

    Added showMouse script to show mouse for the ending scene

---
+++ BeachHackMaze/Assets/Scripts/showMouse.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class showMouse : MonoBehaviour
+{
+    void Update()
+    { 
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+}
